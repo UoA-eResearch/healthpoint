de13acefcdb667191c793b7cdd062d36e5d17462
Nick Young <nick.young@auckland.ac.nz>
2025-02-18 17:16:28 +1300

    fix urls

--- fetch_GPs.py
+++ fetch_GPs.py
@@ -4,5 +4,6 @@ import pandas as pd
 import requests
 
 df = pd.DataFrame(requests.get("https://healthpoint.co.nz/geo.do?zoom=18&branch=gps-accident-urgent-medical-care").json()["results"])
+df["url"] = "https://www.healthpoint.co.nz" + df["url"]
 print(df)
 df.to_csv("GPs.csv", index=False)
